089c5de727f7b4e13d0cae439b7656b0bf5ed98e
Chloe Bethel <chloe@9net.org>
2024-03-06 11:51:22 +0000

    proper serial console configuration

--- nixos-flake/boot-configuration.nix
+++ nixos-flake/boot-configuration.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
   # Serial console
-  boot.kernelParams = [ "console=ttyS0,115200n8" "console=ttyAMA0,115200n8" "console=tty0" ];
+  boot.kernelParams = [ "console=ttyS2,1500000" "earlycon=uart8250,mmio32,0xff130000" ];
 
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
