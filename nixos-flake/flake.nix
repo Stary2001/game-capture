f2e3f787147c5140daae4432f0aa17acb3f029cc
Chloe Bethel <chloe@9net.org>
2024-04-07 15:43:50 +0100

    force mac addresses

--- nixos-flake/flake.nix
+++ nixos-flake/flake.nix
@@ -8,15 +8,14 @@
   };
 
   outputs = inputs:
-    let system = "x86_64-linux";
-    in {
+    {
       formatter.x86_64-linux =
         inputs.nixpkgs.legacyPackages.x86_64-linux.nixfmt;
       formatter.aarch64-linux =
         inputs.nixpkgs.legacyPackages.aarch64-linux.nixfmt;
 
-      nixosConfigurations.default = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
+      nixosConfigurations.vita = inputs.nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
         specialArgs = { inherit inputs; };
         modules = [
           (import ./hardware-configuration.nix)
@@ -28,6 +27,23 @@
           inputs.common.nixosModules.base
           inputs.common.nixosModules.avahi
           inputs.common.nixosModules.wait-online-any
+          ({...}: { networking.hostName = "vita"; networking.interfaces.end0.macAddress = "de:47:97:c1:4d:bc"; networking.interfaces.enu1.macAddress = "de:47:97:c1:4d:bd"; })
+        ];
+      };
+      nixosConfigurations.wii = inputs.nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          (import ./hardware-configuration.nix)
+          (import ./nanopi-r2s.nix)
+          (import ./nginx.nix)
+          (import ./go2rtc.nix)
+          (import ./capture.nix)
+
+          inputs.common.nixosModules.base
+          inputs.common.nixosModules.avahi
+          inputs.common.nixosModules.wait-online-any
+          ({...}: { networking.hostName = "wii"; networking.interfaces.end0.macAddress = "c2:de:b4:2a:cc:2a"; networking.interfaces.enu1.macAddress = "c2:de:b4:2a:cc:2b"; })
         ];
       };
     };
