d4fb94e4bc08fa6b805678a8a78e6156ab2f77c1
Chloe Bethel <chloe@9net.org>
2024-04-11 17:19:10 +0100

    make wiiu use the right device

--- nixos-flake/flake.nix
+++ nixos-flake/flake.nix
@@ -27,7 +27,12 @@
           inputs.common.nixosModules.base
           inputs.common.nixosModules.avahi
           inputs.common.nixosModules.wait-online-any
-          ({...}: { networking.hostName = "vita"; networking.interfaces.end0.macAddress = "de:47:97:c1:4d:bc"; networking.interfaces.enu1.macAddress = "de:47:97:c1:4d:bd"; })
+          ({...}: {
+            networking.hostName = "vita";
+            networking.interfaces.end0.macAddress = "de:47:97:c1:4d:bc";
+            networking.interfaces.enu1.macAddress = "de:47:97:c1:4d:bd"; 
+            services.go2rtc.settings.streams.vita = "ffmpeg:device?video=/dev/v4l/by-id/usb-MACROSILICON_USB_Video-video-index0&resolution=1920x1080&input_format=mjpeg#video=copy";
+          })
         ];
       };
       nixosConfigurations.wii = inputs.nixpkgs.lib.nixosSystem {
@@ -43,9 +48,15 @@
           inputs.common.nixosModules.base
           inputs.common.nixosModules.avahi
           inputs.common.nixosModules.wait-online-any
-          ({...}: { networking.hostName = "wii"; networking.interfaces.end0.macAddress = "c2:de:b4:2a:cc:2a"; networking.interfaces.enu1.macAddress = "c2:de:b4:2a:cc:2b"; })
+          ({...}: {
+            networking.hostName = "wii";
+            networking.interfaces.end0.macAddress = "c2:de:b4:2a:cc:2a";
+            networking.interfaces.enu1.macAddress = "c2:de:b4:2a:cc:2b"; 
+            services.go2rtc.settings.streams.wii = "ffmpeg:device?video=/dev/v4l/by-id/usb-MACROSILICON_USB_Video-video-index0&resolution=1920x1080&input_format=mjpeg#video=copy";
+          })
         ];
       };
+
       nixosConfigurations.wiiu = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
@@ -58,7 +69,10 @@
           inputs.common.nixosModules.base
           inputs.common.nixosModules.avahi
           inputs.common.nixosModules.wait-online-any
-          ({...}: { networking.hostName = "wiiu"; })
+          ({...}: {
+            networking.hostName = "wiiu";
+            services.go2rtc.settings.streams.wiiu = "ffmpeg:device?video= /dev/v4l/by-id/usb-VXIS_Inc_FHD_Capture-video-index0&input_format=yuyv422&video_size=1920x1080#video=h264#hardware=vaapi";
+          })
         ];
       };
     };
